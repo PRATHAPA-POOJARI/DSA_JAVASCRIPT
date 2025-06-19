206d3c79b98d192607134e4197135d364af050cc
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-06-19 19:45:55 +0530

    Create 1.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/asyn-syn/1.js
@@ -0,0 +1,13 @@
+function asyncFunction() {
+    console.log("Step 1");
+  
+    setTimeout(() => {
+      console.log("Step 2 (after 2 seconds)");
+    }, 0); // Runs AFTER 2 seconds
+  
+    console.log("Step 3");
+  }
+  
+  asyncFunction();
+  console.log("Step 4");
+  
