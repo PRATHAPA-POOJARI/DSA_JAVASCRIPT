584da7f22d5a38e2a99ee9444a8c1a39322f91ed
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-06-22 22:28:22 +0530

    Create 3.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/Basics/3.js
@@ -0,0 +1,14 @@
+// 3. Write a JavaScript function to calculate the sum of two numbers
+
+function pk(a ,b) {
+return a+b;
+}
+pk();
+function pk(a ,b) {
+return a+b;
+}
+pk();
+
+let sp = pk(1,4 )
+console.log(sp)
+
