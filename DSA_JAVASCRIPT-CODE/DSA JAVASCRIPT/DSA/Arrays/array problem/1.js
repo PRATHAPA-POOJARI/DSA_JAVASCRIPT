e69d15adb0cac777e16b28a5c295b8ccfebfb093
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-06-23 21:31:27 +0530

    Create 1.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/1.js
@@ -0,0 +1,25 @@
+
+// // find the smallest element in the array.
+   
+// let array = [30,34,21,45,67];
+// let min = array[0];
+// for(let i =0;i<array.length;i++){
+// if(array[i]<min)
+//    {
+//    min = array[i]
+// }
+
+// }
+   
+// console.log(`the smallest element is ${min}`)
+
+let pk = [20,40,1,34,34]
+
+let min = pk [0];
+for(let i =0;i<=pk.length;i++) {
+   if(pk[i]<min)
+
+      min = pk[i]
+}
+
+console.log(min)
