c8cf03239997dd72d5b323d0eec63883562c8263
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-06-26 21:37:15 +0530

    Create 2.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/class/2.js
@@ -0,0 +1,16 @@
+
+class Prathap  {
+constructor(name,native,state,ph){
+    this.name = name;
+    this.native=native;
+    this.state=state;
+    this.ph =ph;
+}
+display() {
+    console.log(`values:${this.name} ${this.native} ${this.state} ${this.ph}`);
+}
+}
+
+const pk = new Prathap('raju','banglore','orissa',856626565);
+
+pk.display();
