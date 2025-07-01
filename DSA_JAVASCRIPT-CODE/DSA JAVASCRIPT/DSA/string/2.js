3f45cf598da70d93db8948a910139f616e92dd83
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-01 22:45:36 +0530

    Create 2.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/string/2.js
@@ -0,0 +1,9 @@
+// Ques 1:  Given an input string  s,   reverse the order of  the words 
+
+// Input :  'the sky is blue ' ->->->->  output: blue is sky the
+//Input : "hello  world"        ->->->   output : world helo
+//Input : "  a good example"    ->->->   output: example good a"
+
+
+//'the sky is blue' =>[the,sky,is,blue]
+//  [] => [the,sky,is,blue] =>  blue is sky the
