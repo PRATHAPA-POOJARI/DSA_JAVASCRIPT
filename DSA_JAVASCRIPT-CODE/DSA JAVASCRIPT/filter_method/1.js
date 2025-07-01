e1fc137ceede613dcc798fc101f5fcf0b5a0a3ae
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-01 22:46:15 +0530

    Create 1.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/filter_method/1.js
@@ -0,0 +1,39 @@
+// const product = [
+//     {
+//         name:'laptop',
+//         name:'phone',
+//         price:'tablet'
+//     },
+//      {
+//         name:'rajesh',
+//         name:'harish',
+//         price:'252'
+//     },
+// ]
+// const searchTerm ='e';
+
+// const result = product.filter(product =>
+//     product.name.toLowerCase().includes(searchTerm.toLowerCase())
+// )
+// console.log(result)
+
+// array.filter((element, index, array) => {
+//   // return true to keep the element
+// })
+
+
+
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const  pk = numbers.filter(even)
+const mk = numbers.filter(odd)
+console.log(pk)
+console.log(mk)
+
+function even ( num){
+    return num % 2 ===0
+}
+
+function odd (num){
+    return num % 2 !==0
+}
