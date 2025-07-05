154530aabb33c19c063ea2f0af602cc2813952e6
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-05 22:20:13 +0530

    Create 5.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/HOF/5.js
@@ -0,0 +1,12 @@
+const fruits = ['Apple', 'Banana', 'Cherry'];
+
+
+function FruitList() {
+  return (
+    <ul>
+      {fruits.map((fruit) => (
+        <li>{fruit}</li>
+      ))}
+    </ul>
+  );
+}
