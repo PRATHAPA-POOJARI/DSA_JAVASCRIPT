ae0147a789123c353b79c30a69327a7d08be60b9
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-06 23:09:25 +0530

    Create 3.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/OOPS/object/3.js
@@ -0,0 +1,40 @@
+//    //3 using an constructor  function 
+   
+//    function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+//     this.greet = function () {
+//       console.log(`Hello, my name is ${this.name}`);
+//     };
+//   }
+  
+//   const person1 = new Person("Alice", 25);
+//   const person2 = new Person("Bob", 28);
+  
+//   person1.greet(); // Output: Hello, my name is Alice
+//   person2.greet(); // Output: Hello, my name is Bob
+  
+  
+
+// function pk(){
+
+//     a = "allsam"
+//     b = "sakdmsa"
+//     console.log('dsjnd')
+//     console.log(a,b)
+// }
+
+// return pk()
+
+
+class Pk {
+ constructor (name ,address){
+    this.n = name;
+    this.a = address;
+ 
+}
+
+}
+const  obj = new Pk('alice',"dsadsa")
+ 
+console.log(obj.n,obj.a)
