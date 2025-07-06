51b2a2293fa051f148340e1c9f902bd0a460a5d1
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-06 23:08:56 +0530

    Create 1.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/OOPS/object/1.js
@@ -0,0 +1,14 @@
+// //  2. using object literals
+
+// const sp = {
+//     name:" prathap ",
+//     native:" banglore",
+//     greet() {
+//         console.log(`my name is${this.name} native is${this.native}`);
+
+//     }
+// };
+
+// sp.greet();
+
+
