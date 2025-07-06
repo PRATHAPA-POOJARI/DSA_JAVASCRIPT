9c9ccc95ad81adfa0f97d3585e5d87f968d6a4f0
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-06 23:09:13 +0530

    Create 4.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/OOPS/object/4.js
@@ -0,0 +1,15 @@
+// // useing object assigbn method 
+
+// const obj1 = { name: "Alice" };
+// const obj2 = { age: 25 };
+// const person = Object.assign({}, obj1, obj2);
+
+// console.log(person); 
+
+
+let obj1 = { name:'sdfsfs'}
+
+let obje2  ={ name :"sksmxc"}
+
+let obj3 = { object.assign({}, obj1,obj2)}
+console.log(obj3)
