d3222a7cf2b9ebe9eb410fda2b901242e5927b3d
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-10 22:46:40 +0530

    Create 1.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/storage/local_storage/1.js
@@ -0,0 +1,11 @@
+// Save
+localStorage.setItem('username', 'John');
+
+// Get
+const user = localStorage.getItem('username'); // 'John'
+
+// Remove
+localStorage.removeItem('username');
+
+// Clear all
+localStorage.clear();
