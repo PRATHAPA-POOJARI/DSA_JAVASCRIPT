2472a5bcd9e3d3449d338997e0dc43fe6e653ece
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-14 22:18:29 +0530

    Create 1.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/storage/session_storage/1.js
@@ -0,0 +1,11 @@
+// Save
+sessionStorage.setItem('step', '2');
+
+// Get
+const step = sessionStorage.getItem('step'); // '2'
+
+// Remove
+sessionStorage.removeItem('step');
+
+// Clear all
+sessionStorage.clear();
