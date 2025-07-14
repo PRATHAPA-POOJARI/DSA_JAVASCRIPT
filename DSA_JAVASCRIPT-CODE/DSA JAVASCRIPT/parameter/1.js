35509be971b3c3be847c7a41fc7aa8366f6f84fc
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-14 22:17:37 +0530

    Create 1.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/parameter/1.js
@@ -0,0 +1,7 @@
+function greet(name, age) { // `name` and `age` are parameters
+    console.log(`Hello, ${name}! You are ${age} years old.`);
+}
+
+greet('Alice', 25); // 'Alice' and 25 are arguments
+
+
