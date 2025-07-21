a79d376b71393023fb098f69508dff3d3d7c71aa
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2025-07-21 21:45:38 +0530

    Create find_large.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/beginer_level_ask/find_large.js
@@ -0,0 +1,7 @@
+// Example array of numbers
+const numbersArray = [5, 2, 9, 1, 7];
+
+// Using Math.max and spread operator to find the largest number
+const largestNumber = Math.max(...numbersArray);
+
+console.log("The largest number is:", largestNumber);
