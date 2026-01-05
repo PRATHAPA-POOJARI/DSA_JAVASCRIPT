b4e38dd3f2b541c59a93d7cf1002ef3b81f154fc
prathappoojari607 <prathappoojari607@gmail.com>
2026-01-05 22:35:16 +0530

    Update 1.js

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/Basics/1.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/Basics/1.js
@@ -1,19 +1,37 @@
 //to check whether given no is prime or not
 
-let p = 1;
-let isPrime = true;
+// let p = 1;
+// let isPrime = true;
 
-for(let i =2;i<p;i++){
-    if(p % i){
-        isPrime = false;
+// for(let i =2;i<p;i++){
+//     if(p % i){
+//         isPrime = false;
+//         break;
+//     }
+// }
+
+
+// if(isPrime){
+//     console.log(" the given value is Prime");
+
+// }
+// else {
+//     console.log(" the given value is not prime")
+// }
+
+let pk = 5;
+let isPrime = true;
+for(let i = 2;i<pk;i++){
+    if(pk % i ===0){
+        isPrime =false;
         break;
     }
 }
 
 if(isPrime){
-    console.log(" the given value is Prime");
-
+    console.log("given number is prime")
 }
 else {
-    console.log(" the given value is not prime")
+  console.log('given number is not prime')
 }
+
