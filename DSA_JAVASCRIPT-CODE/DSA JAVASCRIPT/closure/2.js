8bf3d7ae1eb346bdc27a235f823c471e1b2c3eda
prathappoojari607 <prathappoojari607@gmail.com>
2026-01-08 22:39:34 +0530

    Create 2.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/closure/2.js
@@ -0,0 +1,26 @@
+ 
+
+//  function pk (){
+//     let sp = ' hai';
+    
+//     function kp (){
+//         console.log(sp)
+        
+//     }
+//     return kp
+
+//  }
+//  let mp  = pk();
+//  mp()
+
+function sp () {
+    let pk  = ' hai man';
+
+    function jp(){
+        console.log(pk)
+    }
+    return  jp
+}
+
+let final = sp();
+ final();
