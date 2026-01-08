e2c10e0237408d987b6dd66004b59de7ffdaf6ff
prathappoojari607 <prathappoojari607@gmail.com>
2026-01-08 22:39:36 +0530

    Update 1.js

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/1.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/1.js
@@ -22,15 +22,72 @@
 // }
 // console.log(small)               
 
-let arr = [23,45,67,77];
+// let sp = [2,4,5,6,5,1];
+// let  small = sp[0];
+// for(let i =0;i<sp.length;i++) {
+//     if(sp[i]<small) {
+//         small =sp[i]
+//     }
+// }
+// console.log(`the smalest element in the array is ${small}`)
 
-let small = arr[0];
 
 
-for(let i =0;i<arr.length;i++) {
 
-    if(arr[i]<small){
-        small = arr[i]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr =[34,4,5,4,6,2];
+// let small = arr[0];
+
+// for(let i =0;i<arr.length;i++){
+// if(arr[i]<small);
+//  small =arr[i]
+ 
+
+// }
+
+// console.log(small)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let arr = [2,3,4,500,22,23,0];
+
+let  large = arr[0];
+
+for(let i =0;i<=arr.length-1;i++){
+    if(arr[i]<large){
+        large = arr[i]
     }
-}
-console.log(small)
+};
+
+
+console.log(large)
