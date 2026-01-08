0722ebbcbd5ab70b64c749775588803c185bec88
prathappoojari607 <prathappoojari607@gmail.com>
2026-01-08 22:39:44 +0530

    Update 12.js

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/12.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/12.js
@@ -1,9 +1,31 @@
-let pk = [3,4,55,3,333,333,35,5];
-let unique = [];
-for(i=0;i<pk.length;i++){
-if(!unique.includes(pk[i])) {
-unique.push(pk[i])
-}}
-console.log(unique)
+
+// let pk = [3,4,55,3,333,333,35,5];
+// let unique = [];
+// for(i=0;i<pk.length;i++){
+// if(!unique.includes(pk[i])) {
+// unique.push(pk[i])
+// }}
+// console.log(unique)
 
 
+
+
+
+
+
+
+
+
+
+ let  sp = [2,5,6,67,5,4,44,,66,66];
+
+ let unique = [];
+
+ for(let i = 0;i<sp.length;i++){
+
+    if(!unique.includes(sp[i])){
+        unique.push(sp[i])
+    }
+ }
+
+ console.log(`hey man ${unique}`)
