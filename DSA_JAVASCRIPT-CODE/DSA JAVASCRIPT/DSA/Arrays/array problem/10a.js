f6f8801e82af961dec894110448dceba87b2f793
prathappoojari607 <prathappoojari607@gmail.com>
2026-01-08 22:39:41 +0530

    Update 10a.js

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/10a.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/10a.js
@@ -12,13 +12,25 @@
 
 // store in array formate
 
-let  pk = [34,3,3,4,5,6,7]
-let sp  = [];
+// let  pk = [34,3,3,4,5,6,7]
+// let sp  = [];
 
-for(let i =0;i<pk.length;i++){
-if(pk[i]% 2 ==0){
-    sp.push(pk[i])
-}
+// for(let i =0;i<pk.length;i++){
+// if(pk[i]% 2 ==0){
+//     sp.push(pk[i])
+// }
+// }
+
+// console.log(sp)
+
+
+let sp = [33,44,55,66,3,4,5,6];
+let prime = [];
+
+for(let i =0;i<sp.length;i++){
+    if(sp[i]%2 ==0){
+        prime.push(sp[i])
+    }
 }
 
-console.log(sp)
+console.log(prime)
