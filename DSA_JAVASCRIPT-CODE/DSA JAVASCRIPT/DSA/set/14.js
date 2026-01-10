d414199bef2f5a420d765c4dc6f966090b7daccc
prathappoojari607 <prathappoojari607@gmail.com>
2026-01-10 16:46:51 +0530

    Create 14.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/set/14.js
@@ -0,0 +1,9 @@
+// Check whether number 30 exists in a Set.
+
+let sp = [23,4,34,34,334,30,4554];
+
+let  value = new Set(sp);
+ if(value.has(30)) {
+    console.log('exist')
+ }
+console.log(value)
