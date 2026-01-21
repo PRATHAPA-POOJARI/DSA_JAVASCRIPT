f2367ea463aa9341a18611e7256aefff0539cf49
prathappoojari607 <prathappoojari607@gmail.com>
2026-01-21 23:03:03 +0530

    Create 16.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/set/16.js
@@ -0,0 +1,8 @@
+// Find the size of a Set created from [10, 20, 30, 40].
+
+let arr = [10, 20, 30, 40,30,10];
+let pk  = new Set(arr);
+let value = pk.size;
+console.log(pk.size)
+
+console.log(value)
