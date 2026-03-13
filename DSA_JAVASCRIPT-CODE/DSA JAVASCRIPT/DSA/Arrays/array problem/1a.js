0226b8dd17b4998001a93c6ec6cf9b7727643eb5
prathappoojari607 <prathappoojari607@gmail.com>
2026-03-13 22:50:13 +0530

    Update 1a.js

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/1a.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/1a.js
@@ -8,20 +8,8 @@
 
 // console.log(` the smallest element in the array is ${sm}`)
 
-
-
-
-
-let pk = [44,55,4444,3,4,5];
-let large = pk[0];
-console.log(large)
-
-for(let i = 0;i<=pk.length;i++){
-
-    if(pk[i]>large){
-        large = pk[i]
-    }
-}
-
-
-console.log(` the largest element in the array ${large}`)
+ let num  = [3,55,1,4,2];
+ let small =num[0];
+ for(let i =0;i<num.length;i++){
+    
+ }
