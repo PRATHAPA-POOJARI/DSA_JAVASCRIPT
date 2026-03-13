96cfee9dd8f269e20ed5a2acdd58c9fc59896a58
prathappoojari607 <prathappoojari607@gmail.com>
2026-03-13 22:49:48 +0530

    Create 3a.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/basic_js_code/3a.js
@@ -0,0 +1,11 @@
+// Check even or odd
+
+let isOdd = function (num) {
+  if (num % 2 !== 0) {
+    console.log("The number is odd");
+  } else {
+    console.log("The number is even");
+  }
+};
+
+isOdd(7);
