52bbab0bd1364afd0b70eb84d90296be1023f1d2
prathappoojari607 <prathappoojari607@gmail.com>
2026-03-14 22:33:52 +0530

    Update examples.js

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/leet_code/examples.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/leet_code/examples.js
@@ -1,14 +1,15 @@
 
-let twoSum = function(nums,target){
-    for(let i = 0;i<=nums.length;i++){
-        for(j = i+1;i<=nums.length;i++){
-            if(nums[i]+nums[j]==target){
-                 return [i,j]
+let twoSum = function(num,target){
+    for(let i =0;i<num.length;i++){
+        for(let j =i+1;j<num.length;j++){
+            if(num[i]+ num[j]==target){
+                return [i,j]
             }
         }
     }
+    
 }
-let  nums =[2,7,7,876,6];
-let  target = [14];
 
-console.log(twoSum(nums,target))
+let num = [2, 6, 8];
+let target = 10;
+console.log(twoSum(num, target));
