c9ff353c7ed381e04e38628788f8e360a5b339f6
prathappoojari607 <prathappoojari607@gmail.com>
2026-03-15 22:27:15 +0530

    check given array sorted or not

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/13.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/Arrays/array problem/13.js
@@ -1,55 +1,22 @@
 // Check if an array is sorted or not
 
-// let sp = [2,1,5,6]
-// let issorted = true;
-
-// for(let i = 0;i<sp.length-1;i++){
-
-//     if(sp[i]>sp[i+1]){
-//         issorted =false;
-//         break
-//     }
-// } 
-// if (issorted) {
-//   console.log("Array is sorted in ascending order");
-// } else {
-//   console.log("Array is NOT sorted");
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let sp = [2,3,4];
-
-let   issorted = true;
-
-for(i =0;i<sp.length-1;i++){
-
-  if(sp[i]>sp[i+1]){
-
-    issorted = false
-    break
+function isSorted(arr) {{
+  for(let i=0;i<arr.length-1;i++){
+    if(arr[i]>arr[i+1]){
+      return false;
+    }
   }
+  return true;
+}}
 
-}
+let pk = [1,2,3,4,5];
 
-if(issorted) {
+console.log(isSorted(pk))
 
-  console.log('the given array is sorted')
-}
 
-else {
 
-  console.log(' the  given array is not sorted')
-}
+
+
+
+
 
