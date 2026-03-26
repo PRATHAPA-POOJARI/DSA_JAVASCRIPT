3c31060bb541768b8d98785db9f98fbdf63ff704
prathappoojari607 <prathappoojari607@gmail.com>
2026-03-26 21:50:52 +0530

    Update Q2.js

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/promises/Q2.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/promises/Q2.js
@@ -1,2 +1,11 @@
-// 2. Convert callback to Promise
+// Create a Promise that resolves with "Hello World" and print it using .then()
+
+const promise1 = new  Promise((resolve,reject)=>{
+    resolve ("Hello  world");
+    
+});
+
+promise1.then((pk)=>{
+    console.log(pk)
+})
 
