421bec4256f7917b35b6df1f9e28b563655b1fca
prathappoojari607 <prathappoojari607@gmail.com>
2026-04-07 22:41:20 +0530

    Update Q5.js

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/promises/Q5.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/promises/Q5.js
@@ -1,1 +1,28 @@
-Completed
+
+
+// Write a Promise and use .finally() to print "Completed" regardless of result
+
+let pk = new Promise((resolve,reject)=>{
+    let result = false;
+    if(result){
+        resolve("completed")
+    }
+
+    else {
+
+         reject(" not completed")
+    }
+   
+})
+    console.log(pk)
+    pk
+.then((res)=>{
+    console.log(res)
+})
+.catch((err)=>{
+    console.log(err)
+})
+.finally(()=>{
+console.log("completed")
+});
+
