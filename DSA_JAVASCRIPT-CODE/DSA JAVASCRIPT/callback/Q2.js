4482e2604b97a3f3f9fe2b1ff491e3a2aa41b9a2
prathappoojari607 <prathappoojari607@gmail.com>
2026-04-10 23:18:18 +0530

    Create Q2.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/callback/Q2.js
@@ -0,0 +1,15 @@
+// Pass a callback to print a message after greeting
+
+
+function  kp(nanu){
+    console.log("greeting");
+
+    nanu()
+}
+
+
+function sp(){
+    console.log(" hai janu")
+};
+
+kp(sp)
