1d44f9b2b077a613cef00a90f585e9415e109ac1
prathappoojari607 <prathappoojari607@gmail.com>
2026-04-10 23:18:16 +0530

    Create Q1.js

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/callback/Q1.js
@@ -0,0 +1,14 @@
+// Create a function that accepts a callback and executes it
+
+ let sp = function(janu){
+    console.log(" hai");
+    janu()
+ }
+
+ let kp = function()
+{
+    console.log("nwjn")
+};
+
+sp(kp);
+
