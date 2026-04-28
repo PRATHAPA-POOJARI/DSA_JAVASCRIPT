afb4036251c941ecae249a042e1ef727caa032b3
prathappoojari607 <prathappoojari607@gmail.com>
2026-04-28 23:31:48 +0530

    count the word  in a string..

--- DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/string/10a.js
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/string/10a.js
@@ -1,1 +1,12 @@
-// method 2
+
+// Count words in a string
+let str = "Hello world, how are you?";
+let count = 0;
+
+for (let i = 0; i < str.length; i++) {
+  if (str[i] !== " " && (i === 0 || str[i - 1] === " ")) {
+    count++;
+  }
+}
+
+console.log(count);
