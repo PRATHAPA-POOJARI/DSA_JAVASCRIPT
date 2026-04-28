e02cf12504b9743d7266232447d56a437b70aa9b
prathappoojari607 <prathappoojari607@gmail.com>
2026-04-28 23:32:07 +0530

    count characters

---
+++ DSA_JAVASCRIPT-CODE/DSA JAVASCRIPT/DSA/string/11.js
@@ -0,0 +1,12 @@
+// Count characters (without spaces)
+
+let  pk = " hai nanu  ninu";
+
+let count = 0;
+ for(let i =0;i<pk.length;i++){
+    if(pk[i]!==""){
+        count++
+    }
+ }
+
+ console.log(count)
